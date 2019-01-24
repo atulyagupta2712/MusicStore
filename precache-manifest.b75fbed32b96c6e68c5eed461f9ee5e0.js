b5841c8b52bd309bb853b8e59e792b0c54cd5e44
Atulya <atulyagupta2712@gmail.com>
2019-01-24 17:13:36 +0530

    code

---
+++ precache-manifest.b75fbed32b96c6e68c5eed461f9ee5e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/atulyagupta2712/MusicStore/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "1c52b4e3cce75946ed11",
+    "url": "/atulyagupta2712/MusicStore/static/css/main.e7165554.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/atulyagupta2712/MusicStore/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ab6ab92f074af61e749c",
+    "url": "/atulyagupta2712/MusicStore/static/js/1.ab6ab92f.chunk.js"
+  },
+  {
+    "revision": "62b5a5c4e859737243a9",
+    "url": "/atulyagupta2712/MusicStore/static/js/runtime~main.62b5a5c4.js"
+  },
+  {
+    "revision": "1c52b4e3cce75946ed11",
+    "url": "/atulyagupta2712/MusicStore/static/js/main.1c52b4e3.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/atulyagupta2712/MusicStore/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/atulyagupta2712/MusicStore/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/atulyagupta2712/MusicStore/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ab6ab92f074af61e749c",
+    "url": "/atulyagupta2712/MusicStore/static/css/1.9a1e1eac.chunk.css"
+  },
+  {
+    "revision": "fa9028ab8268a89c9b1367d64eafbe6e",
+    "url": "/atulyagupta2712/MusicStore/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/atulyagupta2712/MusicApplication/precache-manifest.a5b81745240ffebe1fdf8a7798235a45.js"
+  "/atulyagupta2712/MusicStore/precache-manifest.b75fbed32b96c6e68c5eed461f9ee5e0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/atulyagupta2712/MusicApplication/index.html", {
+workbox.routing.registerNavigationRoute("/atulyagupta2712/MusicStore/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
